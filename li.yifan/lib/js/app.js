78e1ed80d6d88a397ca5ccd4b0d882ad8fbb734f
Yifan Li <yifanli@Yifans-MacBook-Pro.local>
2020-10-03 19:19:07 -0700

    frame build

---
+++ li.yifan/lib/js/app.js
@@ -0,0 +1,59 @@
+
+
+$(()=>{
+
+
+   checkUserId();
+
+   // event delegation
+   $(document)
+
+
+
+
+	//$('a').on('click',function(e){
+		//console.log("event data", e)
+	//})
+
+	//$(document)
+	//.on('click','a', function(e){
+		//console.log("event data", e)
+	//})
+
+
+
+   // FORM SUBMITS
+
+   .on("submit","#signin-form",function(e){
+      e.preventDefault();
+      checkSigninForm();
+   })
+
+
+
+
+
+   // ANCHOR CLICKS
+
+   .on("click",'.js-logout',function(e){
+      sessionStorage.removeItem('userId');
+      checkUserId();
+   })
+
+
+
+
+   .on('click','[data-activate]',function(e){
+      let target = $(this).data('activate');
+      $(target).addClass("active")
+   })
+   .on('click','[data-deactivate]',function(e){
+      let target = $(this).data('deactivate');
+      $(target).removeClass("active")
+   })
+   .on('click','[data-toggle]',function(e){
+      let target = $(this).data('toggle');
+      $(target).toggleClass("active")
+   })
+
+})
